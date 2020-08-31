a1dfc57c3a68324771e5ac78d8166fcddfed61c6
sfktrkl <sfktrkl@gmail.com>
2020-09-01 00:53:12 +0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Tutorial!';                   // This is the application title which is {{title}}
   appName = 'appName is Tutorial';       // This is the application name which is {{appName}}
+
+  // These are the properties of the class
+  name = 'Sfktrkl';                      // Angular interpolation
+  // let obj, you cannot use 'let'
+  getName() { return 'Sfktrkl'; }         // Angular function
+  getNameA() { return this.name; }        // Angular function
+  myObj = { name: 'Safak', age: 20 }      // Angular object
+  myArr = ['Sfk', 'trkl'];                // Angular array
+  siteUrl = window.location.href          // Access window object
 }
