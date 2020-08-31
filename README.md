68c49ef6787ffe442b30c5bee8f9f1acf57b3271
sfktrkl <sfktrkl@gmail.com>
2020-09-01 00:02:06 +0300

    Change the project name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Tutorial2
+# Tutorial
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Tutorial2'`, () => {
+  it(`should have as title 'Tutorial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Tutorial2');
+    expect(app.title).toEqual('Tutorial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Tutorial2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Tutorial app is running!');
   });
 });
